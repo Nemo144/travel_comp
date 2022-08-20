11caec7c75e2448d63b7c87f134ddba7949ce50e
Nnamdi Okeke <onnamdialex@gmail.com>
2022-08-20 21:33:50 +0100

    :construction: map component wip

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import googleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
+
+import useStyles from "./styles";
 
 const Map = () => {
-  return <h1>map</h1>;
+  const classes = useStyles();
+
+  //ismobile = false when useMediaQuery > 600px
+  const isMobile = useMediaQuery("(min-width:600px)");
+
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={""}
+        margin={[]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
