2aa71e199e1a59a54a35e58b74d7026cff72246d
Nnamdi Okeke <onnamdialex@gmail.com>
2022-08-22 20:52:46 +0100

    :rocket: map component deployed
    Still needs some work pending the billing issue with google API

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
-import googleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 import Rating from "@material-ui/lab";
@@ -11,18 +10,23 @@ const Map = () => {
   const classes = useStyles();
 
   //ismobile = false when useMediaQuery > 600px
-  const isMobile = useMediaQuery("(min-width:600px)");
+  // const isMobile = useMediaQuery("(min-width:600px)");
 
-  const coordinates = { lat: 0, lng: 0 };
+  const coordinates = { lat: 59.9, lng: 67.8 };
 
   return (
-    <div className={classes.mapContainer}>
+    <div
+      // className={classes.mapContainer}
+      style={{ height: "100vh", width: "100%" }}
+    >
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
+        bootstrapURLKeys={{
+          key: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
+        }}
         defaultCenter={coordinates}
         center={coordinates}
-        defaultZoom={""}
-        margin={[]}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
         options={""}
         onChange={""}
         onChildClick={""}
