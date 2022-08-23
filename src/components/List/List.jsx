14e43d7d85daf9bf87c2b611f00139565fe83256
Nnamdi Okeke <onnamdialex@gmail.com>
2022-08-23 21:14:54 +0100

    :rocket: List component done
    Finished up the list component and also passed props from the PlaceDetails component.

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -8,17 +8,25 @@ import {
   FormControl,
   Select,
 } from "@material-ui/core";
+import PlaceDetails from "../PlaceDetails/PlaceDetails";
 import useStyles from "./styles";
 
 const List = () => {
   const classes = useStyles();
   const [type, setType] = useState("restaurants");
   const [rating, setRating] = useState("");
+
+  const places = [
+    { name: "Cool Place" },
+    { name: "Best Beer" },
+    { name: "Best Steak" },
+  ];
   return (
     <div className={classes.container}>
       <Typography variant>
         Restaurants, Hotels and Attractions around you
       </Typography>
+
       <FormControl className={classes.formControl}>
         <InputLabel>Type</InputLabel>
         <Select value={type} onChange={(e) => setType(e.target.value)}>
@@ -37,6 +45,14 @@ const List = () => {
           <MenuItem value={4.5}>Above 4.5</MenuItem>
         </Select>
       </FormControl>
+
+      <Grid container spacing={3} className={classes.list}>
+        {places.map((place, i) => (
+          <Grid item key={i} xs={12}>
+            <PlaceDetails place={place} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
