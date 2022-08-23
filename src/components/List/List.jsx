0fceb9e6ea511b6a2d1d37d51ad4f47cb1132c87
Nnamdi Okeke <onnamdialex@gmail.com>
2022-08-23 19:38:24 +0100

    :construction: rating fix up

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -12,7 +12,8 @@ import useStyles from "./styles";
 
 const List = () => {
   const classes = useStyles();
-  const [type, setType] = useState();
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
   return (
     <div className={classes.container}>
       <Typography variant>
@@ -24,6 +25,16 @@ const List = () => {
           <MenuItem value="restaurants">Restaurants</MenuItem>
           <MenuItem value="hotels">Hotels</MenuItem>
           <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+      </FormControl>
+
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Above 3.0</MenuItem>
+          <MenuItem value={4}>Above 4.0</MenuItem>
+          <MenuItem value={4.5}>Above 4.5</MenuItem>
         </Select>
       </FormControl>
     </div>
